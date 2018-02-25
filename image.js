f9e77f2532f584820f907996d268b61aee74b10b
Darren Lim <dwlim2@siebl-0222-18.ews.illinois.edu>
2018-02-24 20:29:04 -0600

    sure

---
+++ image.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//   $('.image').click(function() {
+//     $(this).animate({
+//     height: '+=10px'
+//     });
+//   });
+//   $('.image').mouseleave(function() {
+//     $(this).animate({
+//     height: '-=10px'
+//     });
+//   });
+// });
